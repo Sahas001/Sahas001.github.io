285be3bf0eb0098c3e397a511bb0e5c12afb36cc
Sahas Timilsina <tsahas2059@gmail.com>
2025-02-07 16:48:29 +0545

    initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./", // Change from "/" to "./" to fix GitHub Pages MIME issues
+  base: "/", // Change from "/" to "./" to fix GitHub Pages MIME issues
   build: {
     outDir: "dist",
     assetsDir: "assets",
