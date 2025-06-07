147bf6cf36388ba9efe83a6a4e7093da399e1724
Sahas Timilsina <tsahas2059@gmail.com>
2025-06-07 11:04:51 +0545

    style changes

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -7,7 +7,7 @@ type TabsProps = {
 export default function Tabs(props: TabsProps) {
 	return (
 		<button onClick={props.onClick} className={`mb-5 cursor-pointer hover:underline hover:text-white transition duration-200 ${props.highlighted ? "text-purple-400" : ""}`}>
-			<p>[~/{props.name}]$</p>
+			<p>~/{props.name}</p>
 		</button>
 	)
 }
